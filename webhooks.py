fe58bcc3723186de47f2f14176c5fc19468afc22
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2023-06-12 18:12:35 +0200

    Customize messages

--- webhooks.py
+++ webhooks.py
@@ -12,42 +12,39 @@ def webhook():
 
     color = flask.request.args.get("color")
     if color is None:
-        color = 222934
+        color = 0xec6cb9
 
     data = json.loads(flask.request.data.decode())
     try:
         if data["action"] == "created":
             sponsorship = data["sponsorship"]
-            tier_description = sponsorship["tier"]["description"]
             username = sponsorship["sponsor"]["login"]
-            price_in_dollars = int(sponsorship["tier"]["monthly_price_in_cents"])/100
+            tier_name = sponsorship["tier"]["name"]
             supporter_url_html = sponsorship["sponsor"]["html_url"]
             supporter_icon = sponsorship["sponsor"]["avatar_url"]
+            privacy_level = sponsorship["privacy_level"]
             webhook_send_json = {
               "embeds": [
                 {
-                  "description": f"{tier_description}",
                   "color": color,
+                  "title": f"{username} is now {'privately ' if privacy_level == 'private' else ''}sponsoring for {tier_name}",
                   "author": {
-                    "name": f"{username} just sponsored for ${price_in_dollars}",
-                    "url": f"{supporter_url_html}",
-                    "icon_url": f"{supporter_icon}"
-                  },
-                  "footer": {
-                    "text": "Thank you for supporting me, it really helps out"
+                    "name": username,
+                    "icon_url": supporter_icon,
+                    "url": supporter_url_html
                   }
                 }
               ]
             }
             requests.post(f"https://discord.com/api/webhooks/{webhook_id}/{webhook_auth}", json=webhook_send_json)
-            return "Successfully sent webhook to discord!"
+            return "Successfully sent webhook to Discord!"
     except Exception as e:
         if data["hook"]["type"] == "SponsorsListing":
             send_json = {
-                "content": "Github sponsor webhooks have now been added. Thanks for using my repo, starring it would mean to world to me and really helps out. "
+                "content": "GitHub Sponsors webhook has successfully been added! (Using https://github.com/NexInfinite/supporter-discord-webhook)"
             }
             requests.post(f"https://discord.com/api/webhooks/{webhook_id}/{webhook_auth}", json=send_json)
-            return "Setup complete! You will now receive notifications when someone supports you on github! <b>MORE FEATURES ARE COMING SOON</b>"
+            return "Setup complete! You will now receive notifications when someone sponsors you on GitHub!"
         else:
             return e
 
